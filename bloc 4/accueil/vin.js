a4c5287e8902dab959957756034a09fc45d7023d
Leopoldadn <117293869+Leopoldadn@users.noreply.github.com>
2023-01-29 17:44:41 +0100

    MAJ du code html 29/01/2023

---
+++ bloc 4/accueil/vin.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var x = document.getElementById("navbar");
+    if (x.className === "nav"){
+        x.className += " responsive";
+    }
+    else{x.className="nav";}
+}
